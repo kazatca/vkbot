0509429ed71ef8eb00590be259896f5738c908e9
kazatca <kazatca@gmail.com>
2016-09-25 00:33:38 +1000

    change 2captcha usage

--- invite.js
+++ invite.js
@@ -2,23 +2,27 @@ var logger = require('log4js').getLogger()
 var mongo = require('mongodb').MongoClient
 var VK = require('./lib/vk.js');
 var Bot = require('./lib/bot.js')
-var captchaSolver = require('./lib/2captcha.js');
+var CaptchaSolver = require('./lib/2captcha.js');
+
+var captchaSolverLogger = require('log4js').getLogger('2captcha');
+
+captchaSolverLogger.setLevel('TRACE')
 
 
 var dbUrl = process.env[process.env.DB_URL_KEY]
 
 var vk = new VK(process.env.VK_EMAIL, process.env.VK_PASS, process.env.VK_CLIENTID);
 vk.setDb(dbUrl)
-captchaSolver.init(process.env.CAPTCHA_SOLVER_TOKEN)
+var captchaSolver = new CaptchaSolver(process.env.CAPTCHA_SOLVER_TOKEN)
 vk.setCaptchaSolver(captchaSolver);
 
 var bot = new Bot(vk)
 bot.setDb(dbUrl)
 
-var master = 3600733
+// var master = 3600733
 
 var droprope_kms = 44615686
-var event = 126196049
+var event = 129283197
 
 bot.getInviteList(droprope_kms, event)
 .then(function(users) {
